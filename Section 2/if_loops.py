5a223e91cb32eacaf21931b1d839d1d9bd3d0932
Retrojb <baltes.26@osu.edu>
2018-12-05 23:28:03 -0500

    Going to bed commit, working on while loops

--- Section 2/if_loops.py
+++ Section 2/if_loops.py
@@ -9,11 +9,12 @@ if loops
 """
 
 programmer = True
+is_programmer = False
+is_awesome = True
 
 if programmer is True:
     print("Cool dude")
     
-    is_programmer = False
 if is_programmer is True:
     print("poop dick")
 else:
@@ -23,3 +24,10 @@ if is_programmer is False:
     print("Why wouldn't you be")
 else:
     print("You're awesome")
+
+if is_programmer is False or is_awesome is False :
+    print('Way to Go')
+elif is_programmer is False and is_awesome is True : 
+    print('Wow')
+else:
+    print('error')
---
+++ Section 2/while_loop.py
@@ -0,0 +1,44 @@
+"""
+while loops 
+"""
+
+is_programmer = False;
+while not is_programmer:
+    print('Learn programming')
+    user_is_programmer = input('Are you a programmer: ')
+    is_programmer = user_is_programmer == 'yes'
+       
+
+i = 0
+while i < 26:
+    print(f'Repeated {i} times')
+    i = i + 4
+
+temp = 15
+while temp <50:
+    print(f'The temperature is {temp} degrees farenhiet')
+    temp = temp + 3
+
+friends = [
+    {
+        'name' : 'John',
+        'numbers' : {23, 14, 35, 46, 32}
+    },
+     {
+        'name' : 'Bob',
+        'numbers' : {45, 14, 34, 12, 22}
+    },
+     {
+        'name' : 'Chuck',
+        'numbers' : {26, 14, 18, 43, 29}
+    }
+];
+
+#for 'name' in friends:
+print(['name']['numbers'])
+lottery_numbers = { 26, 14, 18, 43, 29 }
+print(' ')
+print(lottery_numbers);
+    
+
+ 
