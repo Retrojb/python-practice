8560b1c9c36872209730f1a5a4bb6e637367e274
Retrojb <baltes.26@osu.edu>
2018-12-11 20:31:10 -0500

    added some to classes

--- Section 3/classes.py
+++ Section 3/classes.py
@@ -1,3 +1,5 @@
+
+"""
 class Garage:
     def __init__(self):
         self.cars = []
@@ -17,3 +19,19 @@ for car in volkswagon:
     print(car)
 
 print(volkswagon[0])
+
+"""
+
+class Club:
+    def __init__(self, name):
+        self.name = name
+        self.players = []
+    def __getitem__(self, i):
+        return self.players[i]
+    def __len__(self):
+        return len(self.players)
+    def __repr__(self):
+        return 'Club {}: {}'.format(self.name, self.players)
+    def __str__(self):
+        return 'Club {} with {} players'.format(self.name, len(self))
+
