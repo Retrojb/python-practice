beaa2a5e0f9ef0d0a233c52ed957417a713ce3db
Retrojb <baltes.26@osu.edu>
2019-02-18 22:30:21 -0500

    working on building a turtle

---
+++ Section 7/quiz_app.py
@@ -0,0 +1,4 @@
+quiz_questions = open('questions.txt', 'r')
+
+for question in quiz_questions:
+    print(f'{question}')
--- practice-files/modules.py
+++ practice-files/modules.py
@@ -1,6 +1,12 @@
 import webbrowser
-
+from random import choice
+from time import time as time_now
 
 #just opens this website from inside the code. could be useful when wanting to launch a webpage on the opening of
-#an application. 
-webbrowser.open('https://google.com')
+#an application.
+#webbrowser.open('https://google.com')
+
+shapes = choice(['square', 'circle', 'trapazoid', 'triangle', 'rectangle', 'rhombus'])
+
+current_time = time_now()
+print(f'At {current_time}, there is a {shapes} siting on the door step')
