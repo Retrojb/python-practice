1b6c99add1d8a989f6985a1dff0a1e4172868595
Retrojb <baltes.26@osu.edu>
2019-02-18 21:37:11 -0500

    webbrowser imports

---
+++ practice-files/modules.py
@@ -0,0 +1,6 @@
+import webbrowser
+
+
+#just opens this website from inside the code. could be useful when wanting to launch a webpage on the opening of
+#an application. 
+webbrowser.open('https://google.com')
