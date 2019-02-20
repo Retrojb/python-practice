315e1bfd6d5d8ccb14df3ca19410b5f8192df818
Retrojb <baltes.26@osu.edu>
2019-02-19 21:30:44 -0500

    fun working art scripts

--- practice-files/dance_animal.py
+++ practice-files/dance_animal.py
@@ -13,30 +13,32 @@ def rectangle(horizontal, vertical, color):
         t.right(90)
     t.end_fill()
     t.penup()
-    t.speed('slow')
-    t.bgColor('red')
-    #feet movement
-    t.goto(-100, -150)
-    rectangle(50, 20, 'blue')
-    t.goto(-30, -150)
-    rectangle(50, 20, 'blue')
-    #legs
-    t.goto(-25, -50)
-    rectangle(15, 100, 'grey')
-    t.goto(-55, -50)
-    rectangle(-15, 100, 'grey')
-    #body
-    t.goto(-90, 100)
-    rectangle(100, 150, 'green')
-    #arms
-    t.togo(-150, 70)
-    rectangle(60,15,'grey')
-    t.goto(-150, 110)
-    rectangle(15, 40, 'grey')
-    #neck
-    t.goto(-50, 120)
-    rectangle(15, 20, 'grey')
-    #head
-    t.goto(-85, 170)
-    rectangle(80, 50, 'green')
+
+t.penup()
+t.speed('slow')
+t.bgcolor('Dodger blue')
+#feet movement
+t.goto(-100, -150)
+rectangle(50, 20, 'blue')
+t.goto(-30, -150)
+rectangle(50, 20, 'blue')
+#legs
+t.goto(-25, -50)
+rectangle(15, 100, 'grey')
+t.goto(-55, -50)
+rectangle(-15, 100, 'grey')
+#body
+t.goto(-90, 100)
+rectangle(100, 150, 'green')
+#arms
+t.goto(-150, 70)
+rectangle(60,15,'grey')
+t.goto(-150, 110)
+rectangle(15, 40, 'grey')
+#neck
+t.goto(-50, 120)
+rectangle(15, 20, 'grey')
+#head
+t.goto(-85, 170)
+rectangle(80, 50, 'green')
 
